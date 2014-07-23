2fc79596353f216133a5cea2c6ccd769cf04da84
Stuart Mumford <stuart@mumford.me.uk>
2014-07-23 19:40:14 +0100

    Length independant attribute read.

--- test_serial_read.F90
+++ test_serial_read.F90
@@ -14,11 +14,10 @@ program test
   character(len=*), parameter :: filename='./test.gdf'
 
 
-  !character(len=20), dimension(:), pointer  :: adata
-  integer(kind=4), dimension(:), pointer :: adata
-  integer(kind=8), dimension(1) :: len
+  real(kind=4), dimension(:), pointer :: adata
+  !integer(kind=8), dimension(1) :: len
 
-  len = (/3/)
+  !len = (/3/)
 
 
 
@@ -35,7 +34,7 @@ program test
 
   call h5gopen_f(file_id, 'simulation_parameters', g_id, error)
 
-  call read_int_attribute(g_id, "domain_dimensions", len, adata)
+  call read_real4_attribute(g_id, "domain_right_edge", adata)
 
   print*, adata
   
@@ -84,7 +83,7 @@ contains
   
 
 
-  subroutine read_int_attribute(g_id, name, len, int_array)
+  subroutine read_int_attribute(g_id, name, int_array)
     
     use hdf5
     use iso_c_binding
@@ -97,17 +96,21 @@ contains
 
     integer(HID_T),                 intent(in) :: g_id      !< group id where to create the attribute
     character(len=*),               intent(in) :: name      !< name
-    integer(HSIZE_T), dimension(1), intent(in) :: len
+    integer(HSIZE_T), dimension(1) :: dims, maxdims
     integer(kind=4), dimension(:),  pointer, intent(out) :: int_array !< the data
     
     integer(HID_T)                            :: aspace_id, attr_id
     integer(kind=4)                           :: error
     type(c_ptr) :: f_ptr
 
-    call h5screate_simple_f(I_ONE, len, aspace_id, error)
+    
     call h5aopen_f(g_id, name, attr_id, error)
+    call h5aget_space_f(attr_id, aspace_id, error)
+    call h5sget_simple_extent_dims_f(aspace_id, dims, maxdims, error)
 
-    allocate(adata(1:len(1)))
+
+    call h5screate_simple_f(I_ONE, dims, aspace_id, error)
+    allocate(adata(1:dims(1)))
     f_ptr = c_loc(int_array(1))
 
     call h5aread_f(attr_id, H5T_NATIVE_INTEGER, f_ptr, error)
@@ -116,4 +119,41 @@ contains
     
   end subroutine read_int_attribute
   
+
+  subroutine read_real4_attribute(g_id, name, int_array)
+    
+    use hdf5
+    use iso_c_binding
+    
+    implicit none
+
+    enum, bind(C)
+       enumerator :: I_ONE = 1, I_TWO
+    end enum
+
+    integer(HID_T),                 intent(in) :: g_id      !< group id where to create the attribute
+    character(len=*),               intent(in) :: name      !< name
+    integer(HSIZE_T), dimension(1) :: dims, maxdims
+    real(kind=4), dimension(:),  pointer, intent(out) :: int_array !< the data
+    
+    integer(HID_T)                            :: aspace_id, attr_id
+    integer(kind=4)                           :: error
+    type(c_ptr) :: f_ptr
+
+    
+    call h5aopen_f(g_id, name, attr_id, error)
+    call h5aget_space_f(attr_id, aspace_id, error)
+    call h5sget_simple_extent_dims_f(aspace_id, dims, maxdims, error)
+
+
+    call h5screate_simple_f(I_ONE, dims, aspace_id, error)
+    allocate(adata(1:dims(1)))
+    f_ptr = c_loc(int_array(1))
+
+    call h5aread_f(attr_id, H5T_NATIVE_REAL, f_ptr, error)
+    call h5aclose_f(attr_id, error)
+    call h5sclose_f(aspace_id, error)
+    
+  end subroutine read_real4_attribute
+
 end program test
